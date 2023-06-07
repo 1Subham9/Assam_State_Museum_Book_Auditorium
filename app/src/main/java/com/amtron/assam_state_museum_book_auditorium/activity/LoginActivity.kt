121ab6898ab06b36a04823dce5f4587676fb9f19
Subham Kumar Ahir <2015dib0030@gmail.com>
2023-06-07 13:58:30 +0530

    Adding Fragments

--- app/src/main/java/com/amtron/assam_state_museum_book_auditorium/activity/LoginActivity.kt
+++ app/src/main/java/com/amtron/assam_state_museum_book_auditorium/activity/LoginActivity.kt
@@ -1,12 +1,25 @@
 package com.amtron.assam_state_museum_book_auditorium.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.amtron.assam_state_museum_book_auditorium.R
+import com.amtron.assam_state_museum_book_auditorium.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.buttonProceed.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
     }
 }
