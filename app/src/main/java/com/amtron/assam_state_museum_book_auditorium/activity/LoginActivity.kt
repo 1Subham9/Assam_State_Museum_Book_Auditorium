5b69388b5a4f272ae725217155be81446e4fbeeb
Subham Kumar Ahir <2015dib0030@gmail.com>
2023-06-07 12:17:46 +0530

    Initial commit

---
+++ app/src/main/java/com/amtron/assam_state_museum_book_auditorium/activity/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.amtron.assam_state_museum_book_auditorium.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.amtron.assam_state_museum_book_auditorium.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
